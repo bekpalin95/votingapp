e659a5d367f9ce4ca0ec1c4fc72f521942f82e22
adambekpalin <adambekpali@gmail.com>
2025-07-05 21:58:43 +0200

    add Poll entity with question and options fields

---
+++ src/main/java/de/adam/votingapp/model/Poll.java
@@ -0,0 +1,29 @@
+package de.adam.votingapp.model;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Poll {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String question;
+
+  @ElementCollection
+  private List<String> options = new ArrayList<>();
+
+  @ElementCollection
+  private List<String> votes = new ArrayList<>();
+
+
+}
