8f004ad636a8fab13d9c95f0c0c17c32f09d6611
adambekpalin <adambekpali@gmail.com>
2025-07-05 20:39:32 +0200

    initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/test/java/de/adam/votingapp/VotingappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adam.votingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
